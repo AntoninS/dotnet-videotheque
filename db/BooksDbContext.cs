165ba3efd9446b0b0fba4716a20ecaa0c49a1c6d
SPONTAK ANTONIN p1501019 <p1501019@univ-lyon1.fr>
2019-04-02 11:59:41 +0200

    Added dbContext

---
+++ db/BooksDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videotheque.db
+{
+    class BooksDbContext : DbContext
+    {
+
+        private static BooksDbContext _context = null;
+        public async static Task<BooksDbContext> GetCurrent()
+        {
+            if (_context == null)
+            {
+                _context = new BooksDbContext(
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "databse.db"));
+                await _context.Database.MigrateAsync();
+            }
+            return _context;
+        }
+
+        private BooksDbContext(string databasePath) : base()
+        {
+            DatabasePath = databasePath;
+        }
+
+        public string DatabasePath { get; }
+
+        //public DbSet<Author> Authors { get; set; }
+        //public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlite($"Filename={DatabasePath}", x => x.SuppressForeignKeyEnforcement());
+        }
+    }
+}
