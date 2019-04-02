b97271b4a2184cc8fa8fae036a771f7686bb0d39
SPONTAK ANTONIN p1501019 <p1501019@univ-lyon1.fr>
2019-04-02 10:49:59 +0200

    Added new classes and enums

---
+++ classes/Personne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videotheque.classes
+{
+    class Personne
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public ECivilite Civilite { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Nationalite { get; set; }
+
+        public DateTime DateNaissance { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
