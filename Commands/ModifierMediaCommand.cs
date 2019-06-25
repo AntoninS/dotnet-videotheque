d2ac9e8e06b55fb0b6b12305cd9710b548dfad8b
Ogün <kocaozogun@gmail.com>
2019-06-25 23:01:49 +0200

    Améliorations de l'affichage et gestions des médias
    Séparations des films et séries, affichage de stats
    Début de la gestion des Média (Ajouter, Modifier, Supprimer)

---
+++ Commands/ModifierMediaCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videotheque.Commands
+{
+    using System.Windows.Input;
+    using videotheque.ViewModel;
+
+    public class ModifierMediaCommand : ICommand
+    {
+        private Action executeMethod;
+        Func<bool> canExecuteMethod;
+
+        public ModifierMediaCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            this.executeMethod = executeMethod;
+            this.canExecuteMethod = canExecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.executeMethod();
+        }
+    }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using videotheque.Commands;
 using videotheque.Model;
 
 namespace videotheque.ViewModel
@@ -10,19 +14,57 @@ namespace videotheque.ViewModel
     class MainWindowViewModel
     {
         
-        public List<Media> Medias { get; set; }
+        public List<Media> ListFilm { get; set; }
+        public List<Media> ListSerie { get; set; }
+
+        public int nbMedia { get; set; }
+
+        public int nbFilms { get; set; }
+
+        public int nbSeries { get; set; }
 
         public MainWindowViewModel()
         {
-            this.LoadMedia();   
+            this.LoadData();
+
+            ModifierLeMediaCommand = new ModifierMediaCommand(executeMethod, canExecuteMethod);
+
+            this.nbMedia = this.ListFilm.Count() + this.ListSerie.Count();
+
+            this.nbFilms = this.ListFilm.Count();
+
+            this.nbSeries = this.ListSerie.Count();
         }
 
-        public async Task LoadMedia()
+        internal bool CanExecute()
+        {
+            return true;
+        }
+
+        public async void LoadData()
         {
             var context = await DataAccess.VideothequeDbContext.GetCurrent();
 
-            this.Medias = context.Medias.ToList();
+            this.ListFilm = context.Medias.Where(m => m.TypeMedia.ToString() == "Film").ToList();
 
+            this.ListSerie = context.Medias.Where(m => m.TypeMedia.ToString() == "Serie").ToList();
+
+        }
+
+        public ICommand ModifierLeMediaCommand
+        {
+            get;
+            set;
+        }
+
+        private bool canExecuteMethod()
+        {
+            return true;
+        }
+
+        private void executeMethod()
+        {
+            Console.WriteLine("No code behind");
         }
 
     }
